aa04a15c8b86a85cc102c7e65ba1d238c7f0158f
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-19 11:05:25 -0600

    added controller and logic to navigate to weather.html

--- app/app.js
+++ app/app.js
@@ -2,12 +2,24 @@ angular
     .module('anguweathar', ['ngRoute'])
     .config(($routeProvider) => {
         {
-            $routeProvider.when('/', {
-                controller: 'RootCtrl',
-                templateUrl: '/partials/root.html'
-            })
+            $routeProvider
+                .when('/', {
+                    controller: 'RootCtrl',
+                    templateUrl: '/partials/root.html'
+                })
+                .when('/weather/:zipcode', {
+                    controller: 'WeatherCtrl',
+                    templateUrl: '/partials/weather.html'
+                })
         }
     })
-    .controller('RootCtrl', function(){
-        
+    .controller('RootCtrl', function($scope, $location){
+        console.log("Using RootCtrl");
+        $scope.gotoWather = () => {
+            //change the url
+            location.href = `/#!/weather/${$scope.zip}`
+        }
+    })
+    .controller('WeatherCtrl', function($scope){
+        console.log("WeatherCtrl");
     })

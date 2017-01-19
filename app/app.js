22ab975127d71b0d0b76b36f728fb570febc019e
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-19 12:22:27 -0600

    created factory and injected into weather controller

--- app/app.js
+++ app/app.js
@@ -15,11 +15,28 @@ angular
     })
     .controller('RootCtrl', function($scope, $location){
         console.log("Using RootCtrl");
-        $scope.gotoWather = () => {
-            //change the url
-            location.href = `/#!/weather/${$scope.zip}`
-        }
+        $scope.gotoWather = () => $location.url(`/weather/${$scope.zip}`)
+
     })
-    .controller('WeatherCtrl', function($scope){
-        console.log("WeatherCtrl");
+    .controller('WeatherCtrl', function($scope, $routeParams, weatherFactory){
+        console.log("Using WeatherCtrl");
+
+        weatherFactory
+            .getWeather($routeParams.zipcode)
+            .then((weather) => {
+                $scope.temperature = weather.temp_f
+                $scope.city = weather.display_location.city
+            })
+    })
+    .factory('weatherFactory', ($http) => {
+        return {
+            getWeather (zipcode) {
+                return $http
+                    .get(`http://api.wunderground.com/api/ed68b3362ec1df3c/conditions/q/${zipcode}.json`)
+                    .then((response) => {
+                        console.log(response.data);
+                        return response.data.current_observation
+                    })
+            },
+        }
     })
